a206e7014caca380908fbc50f27a3f92a6cb4b18
lan64 <lan64@cornell.edu>
2025-09-26 16:32:07 -0400

    delete wrong commits

--- test/dump_bias_child.sv
+++
@@ -1,6 +0,0 @@
-module dump();
-initial begin
-  $dumpfile("waveforms/bias_child.vcd");
-  $dumpvars(0, bias_child); 
-end
-endmodule
--- test/dump_bias_parent.sv
+++
@@ -1,6 +0,0 @@
-module dump();
-initial begin
-  $dumpfile("waveforms/bias_parent.vcd");
-  $dumpvars(0, bias_parent); 
-end
-endmodule
