6feb26038bb22b29aace301c6f1f34c2f4e9fb46
mukundathmaram014 <mukund.athmaram@gmail.com>
2025-12-04 00:31:41 -0500

    improved read me

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # CornellTPU
 The Cornell TPU; or maybe the Big Red Unit (BRU)?
 
-How to use tpu:
+## How to use Cornell TPU:
 
-1. Enter virtual environment : ./venv/Scripts/Activate.ps1
-1. Enter compiler folder: cd compiler
-2. Write your code in tpu_frontend.py file in frontend folder in main function where it says write code (you can use whatever functions are exposed in tpu_txt.py file in backend)
-3. Run frontend:  python -m frontend.tpu_frontend
-4. Run backend: python backend/assembler.py mlp_instruction_trace.txt tpu_instructions.txt
-5. Scp folder onto board (enter password): scp -r .\tpu_deploy\ xilinx@132.236.59.64:~
-6. SSH onto board: ssh xilinx@132.236.59.64
-7. Go into folder copied: cd tpu_deploy
-8. Run code on board: sudo python3 host.py CornellTPU.bit tpu_instructions.txt
-9. Whatever you requested in store commands will be printed.
+1. Enter virtual environment : `./venv/Scripts/Activate.ps1`
+1. Enter compiler folder: `cd compiler`
+2. Write your code in frontend/tpu_frontend.py file in main function where it says write code (you can use whatever functions are exposed in tpu_txt.py file in backend)
+3. Run frontend:  `python -m frontend.tpu_frontend` (generates IR in mlp_instruction_trace.txt)
+4. Run backend: `python backend/assembler.py mlp_instruction_trace.txt tpu_instructions.txt`  (converts IR in mlp_instruction_trace.txt into instructions for TPU in tpu_instructions.txt and auto-generates pynq host.py file. Packages both alongside bitstream in tpu_deploy folder)
+5. Scp tpu_deploy folder onto board: `scp -r .\tpu_deploy\ xilinx@132.236.59.64:~` , (enter password), (you need to be connected to ECE Departmental VPN)
+6. SSH onto board: `ssh xilinx@132.236.59.64` (enter password)
+7. Go into folder copied: `cd tpu_deploy`
+8. Run code on board: `sudo python3 host.py CornellTPU.bit tpu_instructions.txt`
+9. Whatever data you requested in store commands will be printed on terminal.
+10. Once you are done you can delete folder from board:  `cd ..` ,  `rm -rf tpu_deploy`
 
+
+## List of instructions Cornell TPU currently supports:
+
+- load(start_addr, np_array)
+- store(start_addr, length, name)
+- matmul(W, X, Z, m=4)
+- add(X, Y, Z)
+- sub(X, Y, Z)
+- mul(X, Y, Z)
+- relu(X, Zero_addr, Y)
+- relu_derivative(X, Zero_addr, Y)
+
+Information on specific details for each function are defined in function headers for each function in compiler/backend/tpu_txt.py
--- compiler/backend/tpu_txt.py
+++ compiler/backend/tpu_txt.py
@@ -9,13 +9,18 @@ def log_instruction(op, *operands):
 
 def matmul(W, X, Z, m=4):
     """
-    Given two 4x4 input matrices W and X. Does X times W(transposed)
+    Given two mxm input matrices stored contiguously in memory starting
+    at address W and address X, performs X ⋅ W^T and stores data contigously
+    in memory starting at address Z.
+
+    *Z = *X ⋅ (*W)^T
+
     """
     log_instruction("matmul", W, X, Z)
 
 def load(start_addr, np_array):
     """
-    Frontend load operation: request data to be written to BRAM.
+    Loads data in np_array contiguously in memory starting at "start_addr"
     """
     # Convert to numpy array
     if not isinstance(np_array, np.ndarray):
@@ -24,35 +29,52 @@ def load(start_addr, np_array):
     # Flatten 2D or 3D arrays → always 1D
     flat = np_array.astype(np.float32).reshape(-1)
 
-    # The TPU BRAM stores 1 element per address
     length = flat.size
 
-    # Convert to one-line Python literal list
     value_literal = flat.tolist()
 
-    # Write single-line instruction
     log_instruction("load", start_addr, length, value_literal)
 
 def store(start_addr, length, name):
     """
-    Frontend store operation: request data to be read from BRAM
-    and stored in given variable name.
+    Request "length" number of words to be read contigously from memory starting at
+    "start_addr" and prints data under label "name"
     """
     log_instruction("store", start_addr, length, name)
     
 def add(X, Y, Z):
+    """
+    Adds data stored at address X and address Y and stores result in address Z
+    *Z = *X + *Y
+    """
     log_instruction("add", X, Y, Z)
 
 def sub(X, Y, Z):
+    """
+    Subtracts data stored at address Y from data stored at address X and stores result in address Z
+    *Z = *X - *Y
+    """
    log_instruction("sub", X, Y, Z)
 
 def mul(X, Y, Z):
+    """
+    Multiplies data stored at address X and address Y and stores result in address Z
+    *Z = (*X) * (*Y)
+    """
    log_instruction("mul", X, Y, Z)
 
 def relu(X, Zero_addr, Y):
+    """
+    Applies relu function on data stored at address X and stores result in address Y
+    *Y = relu(*X)
+    """
    log_instruction("relu", X, Zero_addr, Y)
 
 def relu_derivative(X, Zero_addr, Y):
+    """
+    Applies relu derivative function on data stored at address X and stores result in address Y
+    *Y = relu'(*X)
+    """
    log_instruction("relu_derivative", X, Zero_addr, Y)
 
 def get_instruction_log():
